cf58d2498fc2e097b113efd4911b90db7fed9356
KevinCCC <wangge2012@gmail.com>
2014-11-15 14:25:58 +0800

    测试

--- main.cpp
+++ main.cpp
@@ -4,11 +4,10 @@
 #include <windows.h>
 int main(int argc, char *argv[])
 {
-    QApplication::addLibraryPath("./plugins");
+   // QApplication::addLibraryPath("./plugins");
     QApplication a(argc, argv);
     Widget w;
     w.setFixedSize(360,115);
     w.show();
-
     return a.exec();
 }
